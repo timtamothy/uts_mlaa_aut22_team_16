9a3976e5d49966dd71bffb0a3500f4bb3b2c1fe4
Tim Wang <45186298+timtamothy@users.noreply.github.com>
2022-05-03 12:23:44 +1000

    Update 02_clean_initial_basic.R

--- 02_clean_initial_basic.R
+++ 02_clean_initial_basic.R
@@ -12,3 +12,25 @@ library(DataExplorer)
 library(funModeling)
 library(Hmisc)
 library(janitor)
+
+# Load Data -----
+
+df1 <- read_csv(here("AT2_credit_train.csv"))
+
+
+# A. Change Pay Amount values to 0 - 1
+
+
+# B. Remove Gender NAs
+
+
+# C. Remove Gender max of 2113
+
+
+# D. Remove Unrealistic Ages or remove the 1 in front of hundred
+
+
+# E. Change Default to 0 and 1
+
+
+# F. Change the type from double to category/factors
